1eeda464005324de9234c1c29d6d132d10850471
arekkas <aeneas@ory.am>
2018-06-12 12:34:08 +0200

    Adds auto-tagging of book.json to circle-ci

---
+++ .circleci/tag.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+cd "$( dirname "${BASH_SOURCE[0]}" )/../guides"
+
+cat book.json | sed 's/[[:space:]]\+\"text\": \"latest (stable)\"/          \"text\": \"latest (stable)\"\
+        },\
+        {\
+          \"value\": \"https:\/\/www.ory.sh\/docs\/guides\/'"${CIRCLE_TAG}"'\",\
+          \"text\": \"'"${CIRCLE_TAG}"'\"/' > book.json
