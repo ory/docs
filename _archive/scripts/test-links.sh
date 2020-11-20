cbf90dceb192a028d3878a78e80f477c1232119c
aeneasr <3372410+aeneasr@users.noreply.github.com>
2020-11-20 15:57:52 +0100

    chore: remove old files

--- _archive/scripts/test-links.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-cd "$( dirname "${BASH_SOURCE[0]}" )/.."
-
-find docs/ -name \*.md -exec ./website/node_modules/.bin/markdown-link-check -c markdown-link-check.json --quiet {} \;
