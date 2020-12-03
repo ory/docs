57f3a50d7cfb517d37ce9761ab25440f1d273b02
aeneasr <aeneas@ory.sh>
2020-12-03 10:29:24 +0000

    autogen: build docs from 6d1c637f3fe32d79adf7e9fad0cab2156574b643

--- docs/config.js
+++ docs/config.js
@@ -3,5 +3,6 @@ module.exports = {
   projectSlug: 'docs',
   projectTagLine: '',
   baseUrl: '/docs/',
-  newsletter: 'https://ory.us10.list-manage.com/subscribe?u=ffb1a878e4ec6c0ed312a3480&id=f605a41b53'
+  newsletter:
+    'https://ory.us10.list-manage.com/subscribe?u=ffb1a878e4ec6c0ed312a3480&id=f605a41b53'
 }
