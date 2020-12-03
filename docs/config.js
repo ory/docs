6d1c637f3fe32d79adf7e9fad0cab2156574b643
hackerman <3372410+aeneasr@users.noreply.github.com>
2020-12-03 11:24:29 +0100

    docs: add newsletter to config

--- docs/config.js
+++ docs/config.js
@@ -2,5 +2,6 @@ module.exports = {
   projectName: 'ORY Ecosystem',
   projectSlug: 'docs',
   projectTagLine: '',
-  baseUrl: '/docs/'
+  baseUrl: '/docs/',
+  newsletter: 'https://ory.us10.list-manage.com/subscribe?u=ffb1a878e4ec6c0ed312a3480&id=f605a41b53'
 }
