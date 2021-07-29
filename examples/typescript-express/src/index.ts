6e3512a2b61635c6da83d565b6eb0cb39933af10
hackerman <3372410+aeneasr@users.noreply.github.com>
2021-07-29 14:17:19 +0200

    docs: document new features and implement better structure (#446)

--- examples/typescript-express/src/index.ts
+++ examples/typescript-express/src/index.ts
@@ -1,55 +1,23 @@
 import express from 'express';
-import {DefaultApi, Configuration} from '@ory/client'
-import jwt from 'express-jwt'
-import jwks from 'jwks-rsa'
 import dotenv from 'dotenv'
+import {protect} from "./middleware";
+import {handleProtected} from "./routes/protected";
+import {handlePublic} from "./routes/public";
 
 // Enable dotenv: https://www.npmjs.com/package/dotenv
 dotenv.config()
-
-// Initializes the Ory SDK
-const ory = new DefaultApi(new Configuration({
-  // Project URL MUST NOT have a trailing slash!
-  basePath: process.env.ORY_PROJECT_URL?.replace(/\/$/, ""),
-  accessToken: process.env.ORY_ACCESS_TOKEN
-}))
-
-// This is the middleware which protects the endpoint. It requires a valid
-// JSON Web Token signed by a key provided by the Ory Proxy.
-const protect = jwt({
-  secret: jwks.expressJwtSecret({
-    cache: true,
-    // This URL is provided by the Ory Proxy and contains the cryptographic keys
-    // for validating the JSON Web Token.
-    jwksUri: 'https://localhost:4000/.ory/jwks.json'
-  }),
-  algorithms: ['ES256']
-})
 
 const app = express();
 
 app.set('view engine', 'pug')
 
-const prettyJson = (json: any) => JSON.stringify(json, null, 2)
-
-// This page is protected
+// This page requires authentication
 app.get('/protected',
   protect,
-  async (req, res) => {
-    const user = req.user as any // Needs a small TypeScript workaround
-
-    // Load the user
-    const identity = (await ory.getIdentityAdmin(user.sub)).data
-
-    res.render('protected', {
-      headers: prettyJson(req.headers),
-      identity: prettyJson(identity),
-      claims: prettyJson(user)
-    })
-  });
+  handleProtected);
 
 // All other URLs show the public page.
-app.use((req, res) => res.render('public'));
+app.use(handlePublic);
 
 const PORT = 8000;
 app.listen(PORT, () => {
