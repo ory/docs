0dc8646af49bd2d036327725ae1023b64280bb85
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-02-08 16:00:43 +0100

    fix: use active project for sdk url

--- code-examples/validate-jwt/nodejs/index.js
+++ code-examples/validate-jwt/nodejs/index.js
@@ -1,8 +1,8 @@
 // This is the middleware which protects the endpoint. It requires a valid
 // JSON Web Token signed by a key provided by the Ory Proxy.
-import jwt from "express-jwt";
-import jwks from "jwks-rsa";
-import {NextFunction, Request, Response} from "express";
+import jwt from 'express-jwt'
+import jwks from 'jwks-rsa'
+import { NextFunction, Request, Response } from 'express'
 
 const jwtMiddleware = jwt({
   secret: jwks.expressJwtSecret({
@@ -16,11 +16,15 @@ const jwtMiddleware = jwt({
     // for validating the JSON Web Token.
     jwksUri: 'http://localhost:4000/.ory/proxy/jwks.json'
   }),
-  algorithms: ['ES256'],
+  algorithms: ['ES256']
 })
 
 // A small wrapper which redirects to login if we are not authenticated.
-export const protect = function (req: Request, res: Response, next: NextFunction) {
+export const protect = function (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   jwtMiddleware(req, res, (err?: any) => {
     // If no error happened, we just call the _next function
     if (!err) {
