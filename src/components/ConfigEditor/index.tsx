6590e97e3aa8679631bde48927c7fa1ef91d58c5
zeropaper <65971+zeropaper@users.noreply.github.com>
2022-02-11 06:54:27 +0100

    feat: add yaml code next to the config editor
    clsoes #572

--- src/components/ConfigEditor/index.tsx
+++ src/components/ConfigEditor/index.tsx
@@ -1,12 +1,21 @@
 import React, { useEffect, useState } from 'react'
-import Form from '@rjsf/core'
+import Form, { IChangeEvent } from '@rjsf/core'
+import CodeBlock from '@theme/CodeBlock'
+import YAML from 'yaml'
 import { Resolver } from '@stoplight/json-ref-resolver'
 import axios from 'axios'
 import RefParser from '@apidevtools/json-schema-ref-parser'
 
+import styles from './index.module.css'
 
 export default function ConfigEditor(props: { schema: any }) {
   const [schema, setSchema] = useState<any>()
+
+  const yamlString = React.useMemo(() => YAML.stringify(schema), [schema])
+
+  const handleChange = (evt: IChangeEvent<any>) => {
+    console.info('handleChange', evt)
+  }
 
   useEffect(() => {
     RefParser.dereference(props.schema, (err, api) => {
@@ -23,9 +32,16 @@ export default function ConfigEditor(props: { schema: any }) {
   }
 
   return (
-    <Form
-      schema={schema}
-      liveValidate={true}
-    />
+    <div className={styles.root}>
+      <Form
+        className={styles.form}
+        onChange={handleChange}
+        schema={schema}
+        liveValidate={true}
+      />
+      <CodeBlock className={styles.yaml} language="yaml">
+        {yamlString}
+      </CodeBlock>
+    </div>
   )
 }
