30fb65d88fd3e6a8108f10223a87bf98eaaa72ad
zeropaper <65971+zeropaper@users.noreply.github.com>
2022-02-11 11:10:25 +0100

    test: add Jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/?(*.)+(test).[jt]s?(x)'],
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.[jt]sx?$': '@swc/jest'
+  }
+}
