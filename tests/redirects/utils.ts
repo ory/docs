100770bc7d49a7b420040c3d84e542827c3c1794
zeropaper <65971+zeropaper@users.noreply.github.com>
2022-02-11 11:10:25 +0100

    test(redirections): check that old URLs get redirected correctly

---
+++ tests/redirects/utils.ts
@@ -0,0 +1,47 @@
+import https from 'https'
+import { readFileSync } from 'fs'
+import axios from 'axios'
+import { resolve, basename } from 'path'
+import { XMLParser } from 'fast-xml-parser'
+
+const sitemapsDir = resolve(__dirname, '../sitemaps')
+
+const parser = new XMLParser()
+
+export const oldAddress = 'https://www.ory.sh/'
+export const newAddress = 'https://docs-omega-six.vercel.app/'
+
+export type Sitemap = {
+  urlset: {
+    url: {
+      loc: string
+    }[]
+  }
+}
+
+interface GetLoc {
+  (url: string, replace?: [string, string]): Promise<{
+    status: number,
+    headers: { [k: string]: any },
+    original: string,
+    redirected: string,
+    destination: string
+  }>
+}
+
+export const getNewURL = (url: string, replace = [oldAddress, newAddress]): string => {
+  return url.replace(replace[0], replace[1])
+}
+
+export const getLoc: GetLoc = async (url, replace = [oldAddress, newAddress]) => {
+  const dest = getNewURL(url, replace)
+  const res = await axios.get(dest)
+  return {
+    ...res,
+    original: url,
+    destination: dest,
+    redirected: `${newAddress}${res.headers.location}`,
+  }
+}
+
+export const readSitemapXML = (filename: string) => parser.parse(readFileSync(resolve(sitemapsDir, filename), 'utf8'))
