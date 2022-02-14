86e66340421eed3c536f5d1be35414956a0411c6
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-02-14 15:30:25 +0100

    test: run all tests concurrently

--- tests/jest/redirects/kratos-redirects.test.ts
+++ tests/jest/redirects/kratos-redirects.test.ts
@@ -3,7 +3,7 @@ import { readSitemapXML, Sitemap, getLoc, getNewURL, maxTimeout } from './utils'
 const sitemap: Sitemap = readSitemapXML('sitemap_kratos.xml')
 
 describe('docs sitemap', () => {
-  it.each(sitemap.urlset.url
+  it.concurrent.each(sitemap.urlset.url
     .map(({ loc }) => [loc, getNewURL(loc)]))('loc %s => %s', async (loc) => {
       const { status } = await getLoc(loc)
       expect(status).toBeLessThan(400)
