1b96c4f59d0f6044f5a04f5f28f8ba08b18d6df3
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-02-24 14:22:03 +0100

    fix: remove obsolete console.log

--- src/static/scripts/redirect.js
+++ src/static/scripts/redirect.js
@@ -1,6 +1,5 @@
 // Needed as a workaround for https://answers.netlify.com/t/trailing-slash-missing-on-proxied-netlify-site/36367
 ;(function () {
-  console.log(location.pathname)
   if (location.pathname === '/docs') {
     window.location = location.href + '/welcome'
   } else if (location.pathname === '/docs/') {
