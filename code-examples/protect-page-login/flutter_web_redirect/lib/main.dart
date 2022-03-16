36d6bc71ac5583e1e69a3542956b4394dca60aad
Alano Terblanche <alanoterblanche@gmail.com>
2022-03-16 11:52:24 +0100

    refactor: cleanup add button

--- code-examples/protect-page-login/flutter_web_redirect/lib/main.dart
+++ code-examples/protect-page-login/flutter_web_redirect/lib/main.dart
@@ -8,7 +8,7 @@ import 'package:flutter_web_redirect/services/auth.dart';
 import 'package:pretty_json/pretty_json.dart';
 
 Future main() async {
-  // load the .env file
+  // load the env file
   await dotenv.load(fileName: "env");
 
   final baseUrl = dotenv.get("ORY_BASE_URL").toString();
@@ -87,25 +87,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
@@ -136,21 +120,9 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text('Session Information:${prettyJson(widget.auth.identity)}'),
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
