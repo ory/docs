0edc5233accd9d887c9aeb64f77a21f2449a00fe
Andrew Minkin <minkin.andrew@gmail.com>
2022-04-15 17:33:28 +0600

    fix: added deploying Kratos to digital ocean guide

--- src/sidebar.js
+++ src/sidebar.js
@@ -223,7 +223,8 @@ module.exports = {
             'kratos/guides/setting-up-password-hashing-parameters',
             'kratos/guides/integration-with-other-systems-using-web-hooks',
             'kratos/guides/tracing',
-            'kratos/guides/upgrade'
+            'kratos/guides/upgrade',
+            'kratos/guides/going-to-production-with-nginx-and-digitalocean'
           ]
         },
         'kratos/reference/api',
