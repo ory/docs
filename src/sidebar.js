b276869740c6df9d15243de93408fe34bb6339ce
Andrew Minkin <minkin.andrew@gmail.com>
2022-04-26 21:21:56 +0600

    fix: added kratos sdk usage (#733)

--- src/sidebar.js
+++ src/sidebar.js
@@ -239,6 +239,9 @@ module.exports = {
           ]
         },
         {
+          SDKs: ['kratos/sdk/index', 'kratos/sdk/go']
+        },
+        {
           'Debug & Help': [
             'kratos/debug/troubleshooting',
             'kratos/debug/csrf',
