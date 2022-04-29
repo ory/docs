b83e074da70737ca29fb8eab46c777af43d67920
tomekpapiernik <29116492+tomekpapiernik@users.noreply.github.com>
2022-04-29 11:59:06 +0200

    chore: apply review comments & more tweaks

--- src/sidebar.js
+++ src/sidebar.js
@@ -83,37 +83,37 @@ module.exports = {
         },
         {
           type: 'category',
+          label: 'Social Sign-in',
+          items: [
+            'guides/social-signin/overview',
+            {
+            'Integrating Providers': [
+              'guides/social-signin/generic',
+              'guides/social-signin/google',
+              'guides/social-signin/facebook',
+              'guides/social-signin/microsoft',
+              'guides/social-signin/github',
+              'guides/social-signin/apple',
+              'guides/social-signin/gitlab',
+              'guides/social-signin/auth0',
+              'guides/social-signin/slack',
+              'guides/social-signin/spotify',
+              'guides/social-signin/discord',
+              'guides/social-signin/twitch',
+              'guides/social-signin/netid',
+              'guides/social-signin/yandex',
+              'guides/social-signin/vk'
+              ]
+            }
+          ]
+        },
+        {
+          type: 'category',
           label: 'Customizing Ory',
           items: [
             'guides/custom-domains',
             'guides/bring-your-user-interface',
             'guides/customize-identity-schema',
-            {
-              type: 'category',
-              label: 'Social Sign-in',
-              items: [
-                'guides/social-signin/overview',
-                {
-                'Integrating Providers': [
-                  'guides/social-signin/google',
-                  'guides/social-signin/facebook',
-                  'guides/social-signin/microsoft',
-                  'guides/social-signin/github',
-                  'guides/social-signin/generic',
-                  'guides/social-signin/apple',
-                  'guides/social-signin/gitlab',
-                  'guides/social-signin/auth0',
-                  'guides/social-signin/slack',
-                  'guides/social-signin/spotify',
-                  'guides/social-signin/discord',
-                  'guides/social-signin/twitch',
-                  'guides/social-signin/netid',
-                  'guides/social-signin/yandex',
-                  'guides/social-signin/vk'
-                  ]
-                }
-              ]
-            },
             'start-building/single-page-app-spa'
           ]
         },
