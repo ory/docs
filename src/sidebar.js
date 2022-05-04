2a0bbc8823778145107d60c4ef59137e4a784983
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2022-05-04 17:04:19 +0200

    fix: custom domains page id

--- src/sidebar.js
+++ src/sidebar.js
@@ -119,7 +119,7 @@ module.exports = {
           type: 'category',
           label: 'Customizing Ory',
           items: [
-            'guides/custom-domains-and-cors',
+            'guides/custom-domains',
             'guides/bring-your-user-interface',
             'guides/customize-identity-schema',
             'start-building/single-page-app-spa'
