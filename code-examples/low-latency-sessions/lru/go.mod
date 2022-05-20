0081f869fb3312c7a88666a7306cd9ed1a17e64a
Andrew Minkin <minkin.andrew@gmail.com>
2022-05-20 15:27:08 +0600

    Added example using ristretto

---
+++ code-examples/low-latency-sessions/lru/go.mod
@@ -0,0 +1,34 @@
+module lru
+
+go 1.17
+
+require (
+	github.com/dgraph-io/ristretto v0.1.0
+	github.com/gin-gonic/gin v1.7.7
+	github.com/ory/kratos-client-go v0.9.0-alpha.3
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.13.0 // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/json-iterator/go v1.1.9 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/ugorji/go/codec v1.1.7 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/oauth2 v0.0.0-20210323180902-22b0adad7558 // indirect
+	golang.org/x/sys v0.0.0-20200930185726-fdedc70b468f // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
---
+++ code-examples/low-latency-sessions/lru/main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/dgraph-io/ristretto"
+	"github.com/gin-gonic/gin"
+
+	client "github.com/ory/kratos-client-go"
+)
+
+const cacheSize = 1000
+
+type kratosMiddleware struct {
+	client *client.APIClient
+	cache  *ristretto.Cache
+}
+
+func NewMiddleware() (*kratosMiddleware, error) {
+	configuration := client.NewConfiguration()
+	configuration.Servers = []client.ServerConfiguration{
+		{
+			URL: "https://playground.projects.oryapis.com",
+		},
+	}
+
+	cache, err := ristretto.NewCache(&ristretto.Config{
+		NumCounters: 1e7,     // number of keys to track frequency of (10M).
+		MaxCost:     1 << 30, // maximum cost of cache (1GB).
+		BufferItems: 64,      // number of keys per Get buffer.
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &kratosMiddleware{
+		client: client.NewAPIClient(configuration),
+		cache:  cache,
+	}, nil
+}
+func (k *kratosMiddleware) Session() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		session, err := k.validateSession(c.Request)
+		if err != nil {
+			c.Redirect(http.StatusMovedPermanently, "https://playground.projects.oryapis.com/ui/login")
+			return
+		}
+		if !*session.Active {
+			c.Redirect(http.StatusMovedPermanently, "http://your_endpoint")
+			return
+		}
+		c.Next()
+	}
+}
+func (k *kratosMiddleware) cacheSession(key string, sess *client.Session) error {
+	// Calculate TTL. Do not cache sessions without TTL.
+	//
+	// Cached session should evict on sess.ExpiresAt
+	exp := (*sess.ExpiresAt).Sub(time.Now())
+	if ok := k.cache.SetWithTTL(fmt.Sprintf("session:%s", key), sess, 1, exp); !ok {
+		return errors.New("cannot set session")
+	}
+	k.cache.Wait()
+	return nil
+}
+
+func (k *kratosMiddleware) getSession(key string) (*client.Session, error) {
+	data, ok := k.cache.Get(fmt.Sprintf("session:%s", key))
+	if !ok {
+		return nil, nil
+	}
+	return data.(*client.Session), nil
+}
+
+func (k *kratosMiddleware) validateSession(r *http.Request) (*client.Session, error) {
+	// Use a cookie name for your Ory Cloud project
+	// It looks like ory_session_projectid
+	cookie, err := r.Cookie("ory_session_playground")
+	if err != nil {
+		return nil, err
+	}
+	if cookie == nil {
+		return nil, errors.New("no session found in cookie")
+	}
+	// Try to return a cached session
+	sess, err := k.getSession(cookie.Value)
+	if err != nil {
+		return nil, err
+	}
+	if sess != nil {
+		return sess, nil
+	}
+	// validate a session
+	resp, _, err := k.client.V0alpha2Api.ToSession(context.Background()).Cookie(cookie.String()).Execute()
+	if err != nil {
+		return nil, err
+	}
+	// cache the session
+	if err := k.cacheSession(cookie.Value, resp); err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+func main() {
+
+	r := gin.Default()
+	k, err := NewMiddleware()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r.Use(k.Session())
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
