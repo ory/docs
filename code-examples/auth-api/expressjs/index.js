21d650318b73a3a5f56e1f52e9e8227a6e742798
tomekpapiernik <29116492+tomekpapiernik@users.noreply.github.com>
2022-08-29 15:18:16 +0200

    chore: test_shouldrunnow

--- code-examples/auth-api/expressjs/index.js
+++ code-examples/auth-api/expressjs/index.js
@@ -24,7 +24,7 @@ app.use(
 )
 
 app.use((req, res, next) => {
-  // A simple middleware to authenticate the request.
+  // A simple middleware to authenticate the request TEST.
   // highlight-start
   ory
     .toSession(
