e7fee89dde67ecc1d012c90455ab87a02f2fef56
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-11-23 13:44:35 +0100

    fix: adjust prism loader

--- src/theme/prism-include-languages.js
+++ src/theme/prism-include-languages.js
@@ -1,30 +1,25 @@
 // Copyright © 2022 Ory Corp
 // SPDX-License-Identifier: Apache-2.0
 
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment"
 import siteConfig from "@generated/docusaurus.config"
+
 import ketoRelationTuplesPrism from "./ketoRelationTuplesPrism"
+export default function prismIncludeLanguages(PrismObject) {
+  const {
+    themeConfig: { prism },
+  } = siteConfig
+  const { additionalLanguages } = prism
+  // Prism components work on the Prism instance on the window, while prism-
+  // react-renderer uses its own Prism instance. We temporarily mount the
+  // instance onto window, import components to enhance it, then remove it to
+  // avoid polluting global namespace.
+  // You can mutate PrismObject: registering plugins, deleting languages... As
+  // long as you don't re-assign it
+  globalThis.Prism = PrismObject
+  additionalLanguages.forEach((lang) => {
+    require(`prismjs/components/prism-${lang}`)
+  })
 
-const prismIncludeLanguages = (PrismObject) => {
-  if (ExecutionEnvironment.canUseDOM) {
-    const {
-      themeConfig: { prism: { additionalLanguages = [] } = {} },
-    } = siteConfig
-    window.Prism = PrismObject
-    additionalLanguages.forEach((lang) => {
-      require(`prismjs/components/prism-${lang}`) // eslint-disable-line
-    })
-
-    ketoRelationTuplesPrism(window.Prism)
-
-    delete window.Prism
-  }
+  ketoRelationTuplesPrism(PrismObject)
+  delete globalThis.Prism
 }
-
-export default prismIncludeLanguages
