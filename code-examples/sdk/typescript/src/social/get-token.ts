ce8e15f76e1bab221e275d1ab35c3e72bfbf6d9e
hackerman <3372410+aeneasr@users.noreply.github.com>
2022-12-05 16:00:12 +0100

    feat: update to sdk v1 (#1137)

--- code-examples/sdk/typescript/src/social/get-token.ts
+++ code-examples/sdk/typescript/src/social/get-token.ts
@@ -1,7 +1,7 @@
 // Copyright © 2022 Ory Corp
 // SPDX-License-Identifier: Apache-2.0
 
-import { Configuration, V0alpha2Api as IdentityApi } from "@ory/client"
+import { Configuration, IdentityApi } from "@ory/client"
 
 const identity = new IdentityApi(
   new Configuration({
@@ -13,7 +13,7 @@ const identity = new IdentityApi(
 export async function getTokens(identityId: string) {
   // highlight-start
   return await identity
-    .adminGetIdentity(identityId, ["oidc"])
+    .getIdentity({ id: identityId, includeCredential: ["oidc"] })
     .then(({ data }) => data)
   // highlight-end
 }
