8865da2fa68979a038a17c3e946da42d868e7e8c
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2022-12-07 17:03:02 +0100

    chore: format

--- docs/kratos/self-service/flows/code/logout/samples/browser/node.ts
+++ docs/kratos/self-service/flows/code/logout/samples/browser/node.ts
@@ -1,17 +1,20 @@
-import { Configuration, FrontendApi } from "@ory/client";
+// Copyright © 2022 Ory Corp
+// SPDX-License-Identifier: Apache-2.0
+
+import { Configuration, FrontendApi } from "@ory/client"
 
 const kratos = new FrontendApi(
   new Configuration({
     basePath: "https://playground.projects.oryapis.com/",
-  })
-);
+  }),
+)
 
 const route = (req: Request, res: Response) => {
   kratos
     .createBrowserLogoutFlow(req.cookies["ory_kratos_session"])
     .then(({ data }) => {
-      console.log(data.logout_url); // The logout URL
-      console.log(data.logout_token); // The logout token
+      console.log(data.logout_url) // The logout URL
+      console.log(data.logout_token) // The logout token
 
       // You can render the logout URL like so:
       // <a href="{{data.logout_url}}>Logout</a>
@@ -20,5 +23,5 @@ const route = (req: Request, res: Response) => {
       // kratos.updateLogoutFlow(data.logout_token).then(() => {
       // Logged out
       // })
-    });
-};
+    })
+}
