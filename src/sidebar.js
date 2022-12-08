82701ae83c8b518661568ac19a0b030168fff054
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2022-12-08 16:12:41 +0100

    chore: move in sidebar

--- src/sidebar.js
+++ src/sidebar.js
@@ -104,6 +104,8 @@ module.exports = {
             },
           ],
         },
+        "kratos/self-service/flows/verify-email-account-activation",
+        "kratos/self-service/flows/account-recovery-password-reset",
         {
           type: "category",
           label: "Managing sessions",
@@ -165,8 +167,6 @@ module.exports = {
             "kratos/self-service/flows/user-registration",
             "kratos/self-service/flows/user-login",
             "kratos/self-service/flows/user-settings",
-            "kratos/self-service/flows/verify-email-account-activation",
-            "kratos/self-service/flows/account-recovery-password-reset",
             "kratos/self-service/flows/user-logout",
             "kratos/self-service/flows/user-facing-errors",
           ],
