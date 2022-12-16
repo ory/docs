ca8b7abb3f7afc364c0463fdbe6066d1a65ab4f2
Ajay Kelkar <ajay.kelkar@ory.sh>
2022-12-16 11:49:12 +0100

    docs: address review comments

--- src/sidebar.js
+++ src/sidebar.js
@@ -305,11 +305,6 @@ module.exports = {
     },
     {
       type: "category",
-      label: "Advanced",
-      items: ["advanced/settings"],
-    },
-    {
-      type: "category",
       label: "Actions",
       items: ["guides/integrate-with-ory-cloud-through-webhooks"],
     },
