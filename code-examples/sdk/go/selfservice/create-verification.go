0ab6364b1f10ae91ebbf993b7a9ed0ce749f30a6
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2023-01-11 17:13:00 +0100

    feat: verification flow

---
+++ code-examples/sdk/go/selfservice/create-verification.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/ory/client-go"
+)
+
+var ory *client.APIClient
+
+func init() {
+	cfg := client.NewConfiguration()
+	cfg.Servers = client.ServerConfigurations{
+		{URL: fmt.Sprintf("https://%s.projects.oryapis.com", os.Getenv("ORY_PROJECT_SLUG"))},
+	}
+
+	ory = client.NewAPIClient(cfg)
+}
+
+func createVerification(ctx context.Context) (*client.VerificationFlow, error) {
+	// highlight-start
+	flow, _, err := ory.FrontendApi.CreateNativeVerificationFlow(ctx).Execute()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return flow, nil
+	// highlight-end
+}
---
+++ code-examples/sdk/go/selfservice/get-verification.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/ory/client-go"
+)
+
+var ory *client.APIClient
+
+func init() {
+	cfg := client.NewConfiguration()
+	cfg.Servers = client.ServerConfigurations{
+		{URL: fmt.Sprintf("https://%s.projects.oryapis.com", os.Getenv("ORY_PROJECT_SLUG"))},
+	}
+
+	ory = client.NewAPIClient(cfg)
+}
+
+func getVerification(ctx context.Context, flowId string) (*client.VerificationFlow, error) {
+	// highlight-start
+	flow, _, err := ory.FrontendApi.GetVerificationFlow(ctx).Id(flowId).Execute()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return flow, nil
+	// highlight-end
+}
