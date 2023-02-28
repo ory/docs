a0e7d813814632fca783355dc8ea6161af606310
Henning F <18215579+CaptainStandby@users.noreply.github.com>
2023-02-28 13:39:29 +0100

    feat: add some documentation for `transient_payload` (#1290)
    * feat: add some documentation for `transient_payload`
    
    * fix typos

--- code-examples/curl/registration/submit-registration-browser.sh
+++ code-examples/curl/registration/submit-registration-browser.sh
@@ -1,7 +1,7 @@
 curl -X POST \
     -H 'Content-Type: application/json' \
     -H 'Accept: application/json' \
-    -d '{"method":"password","csrf_token":"your-csrf-token","traits.email":"email@example.com","password":"verystrongpassword","traits.tos":"true"}' \
+    -d '{"method":"password","csrf_token":"your-csrf-token","traits.email":"email@example.com","password":"verystrongpassword","traits.tos":"true","transient_payload.consents":"newsletter,usage_stats"}' \
     -b cookies.txt \
     -c cookies.txt \
     "https://{project.slug}.projects.oryapis.com/self-service/registration?flow=<your-flow-id>"
