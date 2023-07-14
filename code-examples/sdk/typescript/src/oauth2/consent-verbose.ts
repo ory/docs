2a6406b3097a09137468cd1522164976fe63461f
Vincent <vincent@ory.sh>
2023-07-14 18:35:09 +0200

    fix: consent-verbose code example
    the consent-verbose example was a copy of login-verbose. This patch changes it a correct example code

--- code-examples/sdk/typescript/src/oauth2/consent-verbose.ts
+++ code-examples/sdk/typescript/src/oauth2/consent-verbose.ts
@@ -1,85 +1,175 @@
-// Copyright © 2022 Ory Corp
+// Copyright © 2023 Ory Corp
 // SPDX-License-Identifier: Apache-2.0
 
-import { Configuration, OAuth2Api } from "@ory/client"
-import { Request, Response } from "express"
+import express from "express"
+import url from "url"
+import urljoin from "url-join"
+import csrf from "csurf"
+import { hydraAdmin } from "../config"
+import { oidcConformityMaybeFakeSession } from "./stub/oidc-cert"
+import { AcceptOAuth2ConsentRequestSession } from "@ory/client"
 
-const ory = new OAuth2Api(
-  new Configuration({
-    basePath: `https://${process.env.ORY_PROJECT_SLUG}.projects.oryapis.com`,
-    accessToken: process.env.ORY_API_KEY,
-  }),
-)
+// Sets up csrf protection
+const csrfProtection = csrf({
+  cookie: {
+    sameSite: "lax",
+  },
+})
+const router = express.Router()
 
-function authenticateUserCredentials(email: string, password: string): any {
-  // Example method to authenticate users and fetch them from the DB.
-}
+router.get("/", csrfProtection, (req, res, next) => {
+  // Parses the URL query
+  const query = url.parse(req.url, true).query
 
-// Please note that this is an example implementation.
-// In a production app, please add proper error handling.
-export async function handleLogin(request: Request, response: Response) {
-  const challenge = request.query.login_challenge.toString()
-  const { data: loginRequest } = await ory.getOAuth2LoginRequest({
-    loginChallenge: challenge.toString(),
-  })
+  // The challenge is used to fetch information about the consent request from ORY hydraAdmin.
+  const challenge = String(query.consent_challenge)
+  if (!challenge) {
+    next(new Error("Expected a consent challenge to be set but received none."))
+    return
+  }
 
-  if (loginRequest.skip) {
-    // User is already authenticated, don't show the login form and simply accept the login request.
-    await ory
-      .acceptOAuth2LoginRequest({
-        loginChallenge: challenge,
-        acceptOAuth2LoginRequest: {
-          subject: loginRequest.subject,
-        },
+  // This section processes consent requests and either shows the consent UI or
+  // accepts the consent request right away if the user has given consent to this
+  // app before
+  hydraAdmin
+    .adminGetOAuth2ConsentRequest(challenge)
+    // This will be called if the HTTP request was successful
+    .then(({ data: body }) => {
+      // If a user has granted this application the requested scope, hydra will tell us to not show the UI.
+      // Any cast needed because the SDK changes are still unreleased.
+      // TODO: Remove in a later version.
+      if (body.skip || (body.client as any)?.skip_consent) {
+        // You can apply logic here, for example grant another scope, or do whatever...
+        // ...
+
+        // Now it's time to grant the consent request. You could also deny the request if something went terribly wrong
+        return hydraAdmin
+          .adminAcceptOAuth2ConsentRequest(challenge, {
+            // We can grant all scopes that have been requested - hydra already checked for us that no additional scopes
+            // are requested accidentally.
+            grant_scope: body.requested_scope,
+
+            // ORY Hydra checks if requested audiences are allowed by the client, so we can simply echo this.
+            grant_access_token_audience: body.requested_access_token_audience,
+
+            // The session allows us to set session data for id and access tokens
+            session: {
+              // This data will be available when introspecting the token. Try to avoid sensitive information here,
+              // unless you limit who can introspect tokens.
+              // accessToken: { foo: 'bar' },
+              // This data will be available in the ID token.
+              // idToken: { baz: 'bar' },
+            },
+          })
+          .then(({ data: body }) => {
+            // All we need to do now is to redirect the user back to hydra!
+            res.redirect(String(body.redirect_to))
+          })
+      }
+
+      // If consent can't be skipped we MUST show the consent UI.
+      res.render("consent", {
+        csrfToken: req.csrfToken(),
+        challenge: challenge,
+        // We have a bunch of data available from the response, check out the API docs to find what these values mean
+        // and what additional data you have available.
+        requested_scope: body.requested_scope,
+        user: body.subject,
+        client: body.client,
+        action: urljoin(process.env.BASE_URL || "", "/consent"),
       })
-      .then(({ data }) => response.redirect(data.redirect_to))
-    return
-  }
-
-  // Show the login form if the form was not submitted.
-  if (request.method === "GET") {
-    response.render("login", {
-      loginRequest,
     })
-    return
-  }
+    // This will handle any error that happens when making HTTP calls to hydra
+    .catch(next)
+  // The consent request has now either been accepted automatically or rendered.
+})
 
-  // The user did not want to sign in with the given app.
-  if (request.body.submit === "Deny access") {
-    await ory
-      .rejectOAuth2LoginRequest({
-        loginChallenge: challenge,
-        rejectOAuth2Request: {
+router.post("/", csrfProtection, (req, res, next) => {
+  // The challenge is now a hidden input field, so let's take it from the request body instead
+  const challenge = req.body.challenge
+
+  // Let's see if the user decided to accept or reject the consent request..
+  if (req.body.submit === "Deny access") {
+    // Looks like the consent request was denied by the user
+    return (
+      hydraAdmin
+        .adminRejectOAuth2ConsentRequest(challenge, {
           error: "access_denied",
           error_description: "The resource owner denied the request",
-        },
-      })
-      .then(({ data }) => response.redirect(data.redirect_to))
+        })
+        .then(({ data: body }) => {
+          // All we need to do now is to redirect the browser back to hydra!
+          res.redirect(String(body.redirect_to))
+        })
+        // This will handle any error that happens when making HTTP calls to hydra
+        .catch(next)
+    )
+  }
+  // label:consent-deny-end
+
+  let grantScope = req.body.grant_scope
+  if (!Array.isArray(grantScope)) {
+    grantScope = [grantScope]
   }
 
-  const user = authenticateUserCredentials(
-    request.body.email,
-    request.body.password,
-  )
+  // The session allows us to set session data for id and access tokens
+  let session: AcceptOAuth2ConsentRequestSession = {
+    // This data will be available when introspecting the token. Try to avoid sensitive information here,
+    // unless you limit who can introspect tokens.
+    access_token: {
+      // foo: 'bar'
+    },
 
-  // Check login credentials (e.g. email + password) in your user database.
-  if (user!) {
-    response.render("login", { error: "invalid credentials", loginRequest })
-    return
+    // This data will be available in the ID token.
+    id_token: {
+      // baz: 'bar'
+    },
   }
 
-  // User was authenticated successfully,
-  return await ory
-    .acceptOAuth2LoginRequest({
-      loginChallenge: challenge,
-      acceptOAuth2LoginRequest: {
-        subject: user.id,
-        remember: Boolean(request.body.remember),
-        remember_for: 3600,
-        context: {
-          // You can add any context that you want to be available to the consent endpoint.
-        },
-      },
+  // Here is also the place to add data to the ID or access token. For example,
+  // if the scope 'profile' is added, add the family and given name to the ID Token claims:
+  // if (grantScope.indexOf('profile')) {
+  //   session.id_token.family_name = 'Doe'
+  //   session.id_token.given_name = 'John'
+  // }
+
+  // Let's fetch the consent request again to be able to set `grantAccessTokenAudience` properly.
+  hydraAdmin
+    .adminGetOAuth2ConsentRequest(challenge)
+    // This will be called if the HTTP request was successful
+    .then(({ data: body }) => {
+      return hydraAdmin
+        .adminAcceptOAuth2ConsentRequest(challenge, {
+          // We can grant all scopes that have been requested - hydra already checked for us that no additional scopes
+          // are requested accidentally.
+          grant_scope: grantScope,
+
+          // If the environment variable CONFORMITY_FAKE_CLAIMS is set we are assuming that
+          // the app is built for the automated OpenID Connect Conformity Test Suite. You
+          // can peak inside the code for some ideas, but be aware that all data is fake
+          // and this only exists to fake a login system which works in accordance to OpenID Connect.
+          //
+          // If that variable is not set, the session will be used as-is.
+          session: oidcConformityMaybeFakeSession(grantScope, body, session),
+
+          // ORY Hydra checks if requested audiences are allowed by the client, so we can simply echo this.
+          grant_access_token_audience: body.requested_access_token_audience,
+
+          // This tells hydra to remember this consent request and allow the same client to request the same
+          // scopes from the same user, without showing the UI, in the future.
+          remember: Boolean(req.body.remember),
+
+          // When this "remember" sesion expires, in seconds. Set this to 0 so it will never expire.
+          remember_for: 3600,
+        })
+        .then(({ data: body }) => {
+          // All we need to do now is to redirect the user back to hydra!
+          res.redirect(String(body.redirect_to))
+        })
     })
-    .then(({ data }) => response.redirect(data.redirect_to))
-}
+    // This will handle any error that happens when making HTTP calls to hydra
+    .catch(next)
+  // label:docs-accept-consent
+})
+
+export default router
