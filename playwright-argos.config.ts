883e7bb3ae295d8138b3d43ff047c94a9dc011a9
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-01-08 13:51:25 +0100

    chore: synchronize workspaces

--- playwright-argos.config.ts
+++ playwright-argos.config.ts
@@ -1,20 +1,20 @@
-import {devices} from '@playwright/test';
-import type {PlaywrightTestConfig} from '@playwright/test';
+import { devices } from "@playwright/test"
+import type { PlaywrightTestConfig } from "@playwright/test"
 
 const config: PlaywrightTestConfig = {
   testDir: "./tests/playwright-argos",
   webServer: {
     port: 3000,
-    command: 'npm run serve',
+    command: "npm run serve",
   },
   projects: [
     {
-      name: 'chromium',
+      name: "chromium",
       use: {
-        ...devices['Desktop Chrome'],
+        ...devices["Desktop Chrome"],
       },
     },
   ],
-};
+}
 
-export default config;
+export default config
