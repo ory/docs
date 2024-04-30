656db1acbba636f91d778bf683a952204c574f8f
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2024-04-30 15:47:03 +0200

    chore: format

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,12 +1,9 @@
 import React from "react"
-// Import the original mapper
 import MDXComponents from "@theme-original/MDXComponents"
 import AjaxWarning from "./AjaxWarning"
 
 export default {
   // Re-use the default mapping
   ...MDXComponents,
-  // Map the "<Highlight>" tag to our Highlight component
-  // `Highlight` will receive all props that were passed to `<Highlight>` in MDX
   AjaxWarning,
 }
