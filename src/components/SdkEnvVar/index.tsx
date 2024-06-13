fc76713d84a361f0fe5dafba7efcff41d88d04e4
vinckr <vincent@ory.sh>
2024-06-13 14:51:15 +0200

    fix: SdkEnvVar

--- src/components/SdkEnvVar/index.tsx
+++ src/components/SdkEnvVar/index.tsx
@@ -10,16 +10,16 @@ export default function SdkEnvVar(props: any) {
   return (
     <Tabs>
       <TabItem value="macos" label="macOS" default>
-        <CodeBlock language="shell">{`${hint}export ORY_SDK_URL=${url}`}</CodeBlock>
+        <CodeBlock language="shell">{`${hint}export ORY_PROJECT_SLUG=${url}`}</CodeBlock>
       </TabItem>
       <TabItem value="linux" label="Linux">
-        <CodeBlock language="shell">{`${hint}export ORY_SDK_URL=${url}`}</CodeBlock>
+        <CodeBlock language="shell">{`${hint}export ORY_PROJECT_SLUG=${url}`}</CodeBlock>
       </TabItem>
       <TabItem value="banana" label="Windows CMD">
-        <CodeBlock language="shell">{`${hint}set ORY_SDK_URL=${url}`}</CodeBlock>
+        <CodeBlock language="shell">{`${hint}set ORY_PROJECT_SLUG=${url}`}</CodeBlock>
       </TabItem>
       <TabItem value="powershell" label="Windows Powershell">
-        <CodeBlock language="powershell">{`${hint}$Env:ORY_SDK_URL = "${url}"`}</CodeBlock>
+        <CodeBlock language="powershell">{`${hint}$Env:ORY_PROJECT_SLUG = "${url}"`}</CodeBlock>
       </TabItem>
       <TabItem value="self-hosted" label="Self-Hosted Ory Kratos">
         <p>Clone and run Ory Kratos locally</p>
@@ -29,7 +29,7 @@ git checkout master
 git pull -ff
 docker-compose -f quickstart.yml -f contrib/quickstart/kratos/cloud/quickstart.yml up --build --force-recreate -d`}</CodeBlock>
         <p>and set the environment variable to the exposed port:</p>
-        <CodeBlock language="shell">{`export ORY_SDK_URL=http://localhost:4433`}</CodeBlock>
+        <CodeBlock language="shell">{`export ORY_PROJECT_SLUG=http://localhost:4433`}</CodeBlock>
       </TabItem>
     </Tabs>
   )
