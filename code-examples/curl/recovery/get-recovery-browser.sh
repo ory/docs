41422c7ab1a26923f2f45bc010fe1efc9d99851c
Vincent <vincent@ory.sh>
2024-06-17 11:57:55 +0200

    chore: fix commands (#1727)
    * chore:  in commands
    
    * chore: replace project.slug
    
    * chore: update style guide
    
    * chore: replace more curly
    
    * chore: replace more curly
    
    * chore: replace more curly
    
    * fix: capitalize  and project_slug

--- code-examples/curl/recovery/get-recovery-browser.sh
+++ code-examples/curl/recovery/get-recovery-browser.sh
@@ -2,4 +2,4 @@ curl -H 'Content-Type: application/json' \
     -H 'Accept: application/json' \
     -c cookies.txt \
     -b cookies.txt \
-    "https://{project.slug}.projects.oryapis.com/self-service/recovery/flows?id=<your-flow-id>"
+    "https://$PROJECT_SLUG.projects.oryapis.com/self-service/recovery/flows?id=<your-flow-id>"
--- code-examples/curl/verification/get-verification-browser.sh
+++ code-examples/curl/verification/get-verification-browser.sh
@@ -2,4 +2,4 @@ curl -H 'Content-Type: application/json' \
     -H 'Accept: application/json' \
     -b cookies.txt \
     -c cookies.txt \
-    "https://{project.slug}.projects.oryapis.com/self-service/verification/flows?id=<your-flow-id>"
+    "https://$PROJECT_SLUG.projects.oryapis.com/self-service/verification/flows?id=<your-flow-id>"
