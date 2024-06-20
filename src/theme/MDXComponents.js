d85c2aeb3766108a8631e672add0543dfcda3ef4
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2024-06-20 09:07:09 +0200

    chore: u

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,8 +1,6 @@
 import React from "react"
 import MDXComponents from "@theme-original/MDXComponents"
 import AjaxWarning from "./AjaxWarning"
-// Import the original mapper
-import MDXComponents from "@theme-original/MDXComponents"
 import ConsoleLink from "../components/ConsoleLink/console-link"
 
 export default {
