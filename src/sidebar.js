06cf0ef5c81a03076edfd94bc397768a3c2c72e2
vinckr <vincent@ory.sh>
2024-08-13 16:15:12 +0200

    fix: add projects api guide

--- src/sidebar.js
+++ src/sidebar.js
@@ -329,10 +329,10 @@ module.exports = {
       items: [
         "guides/custom-domains",
         "guides/cors",
+        "guides/manage-project-via-api",
         "guides/gitops",
         "guides/rate-limits",
         "guides/allowlist",
-        "guides/delete-project",
       ],
     },
     {
