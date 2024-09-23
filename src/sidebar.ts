95ee7f9217f004b603956b29c04653209ac71388
hackerman <3372410+aeneasr@users.noreply.github.com>
2024-09-23 13:19:22 +0200

    docs: guide for changing project owners (#1886)

--- src/sidebar.ts
+++ src/sidebar.ts
@@ -971,6 +971,7 @@ module.exports = {
         "guides/manage-project-via-api",
         "guides/custom-domains",
         "console/usage-billing",
+        "console/change-owner",
         "guides/gitops",
       ],
     },
