d4d034a464602414ba6e9ba4d0fa5c76a0dabfc3
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2024-09-24 09:19:49 +0000

    chore: configure auto-block script (#1892)

--- src/plugins/analytics/index.js
+++ src/plugins/analytics/index.js
@@ -7,6 +7,15 @@ module.exports = function (context) {
         headTags: [
           {
             tagName: "script",
+            innerHTML: `
+              window.TERMLY_CUSTOM_BLOCKING_MAP = {
+                "sqa-web.ory.sh": "essential",
+                "static.reo.dev": "advertising",
+              }
+            `
+          },
+          {
+            tagName: "script",
             attributes: {
               type: "text/javascript",
               src: "https://app.termly.io/resource-blocker/3563b8d6-caf2-41c3-a821-9d0eb05a7409?autoBlock=on",
@@ -14,11 +23,9 @@ module.exports = function (context) {
           },
           {
             tagName: "script",
-            attributes: {
-              type: "text/javascript",
-              "data-categories": "essential",
-              src: "https://www.ory.sh/scripts/cookies.js",
-            },
+            innerHTML: `
+            !function(){var e,t,n;e="400b988f083a73b",t=function(){Reo.init({clientID:"400b988f083a73b"})},(n=document.createElement("script")).src="https://static.reo.dev/"+e+"/reo.js",n.async=!0,n.onload=t,document.head.appendChild(n)}();
+            `
           },
         ],
       }
