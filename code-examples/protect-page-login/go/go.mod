10ca63ce0ecfacdbd96d0d0fdc54ae24d12453b2
zepatrik <zepatrik@users.noreply.github.com>
2024-10-04 12:37:28 +0200

    chore: simplify go example app

--- code-examples/protect-page-login/go/go.mod
+++ code-examples/protect-page-login/go/go.mod
@@ -2,6 +2,6 @@ module github.com/ory/docs/code-examples/go
 
 go 1.17
 
-require github.com/ory/client-go v1.11.7
+require github.com/ory/client-go v1.15.5
 
-require golang.org/x/oauth2 v0.21.0 // indirect
+require golang.org/x/oauth2 v0.23.0 // indirect
--- code-examples/protect-page-login/go/handler.go
+++ code-examples/protect-page-login/go/handler.go
@@ -9,22 +9,42 @@ import (
 	"net/http"
 )
 
-func (app *App) dashboardHandler() http.HandlerFunc {
-	return func(writer http.ResponseWriter, request *http.Request) {
-		tmpl, err := template.New("index.html").ParseFiles("index.html")
-		if err != nil {
-			http.Error(writer, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		session, err := json.Marshal(getSession(request.Context()))
-		if err != nil {
-			http.Error(writer, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		err = tmpl.ExecuteTemplate(writer, "index.html", string(session))
-		if err != nil {
-			http.Error(writer, err.Error(), http.StatusInternalServerError)
-			return
-		}
+func (app *App) dashboardHandler(writer http.ResponseWriter, request *http.Request) {
+	session, err := getSession(request.Context())
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	rawSession, err := json.Marshal(session)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = dashboardTemplate.ExecuteTemplate(writer, "index.html", rawSession)
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+var dashboardTemplate *template.Template
+
+func init() {
+	var err error
+	dashboardTemplate, err = template.New("index.html").Parse(`
+<html lang="en">
+  <head>
+    <title>Ory Network secured Go web app</title>
+  </head>
+  <body>
+    <h1>Dashboard</h1>
+    <hr />
+    <h2>Your Session Data:</h2>
+    <code>{{ . }}</code>
+  </body>
+</html>
+`)
+	if err != nil {
+		panic(err)
 	}
 }
