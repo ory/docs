f580854762fcb62300a9c026681f115923891689
Jonas Hungershausen <jonas.hungershausen@ory.sh>
2024-11-12 14:40:58 +0100

    chore: add more data

--- src/plugins/analytics/index.js
+++ src/plugins/analytics/index.js
@@ -28,6 +28,14 @@ module.exports = function (context) {
             !function(){var e,t,n;e="400b988f083a73b",t=function(){Reo.init({clientID:"400b988f083a73b"})},(n=document.createElement("script")).src="https://static.reo.dev/"+e+"/reo.js",n.async=!0,n.onload=t,document.head.appendChild(n)}();
             `
           },
+          {
+            tagName: "script",
+            innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://s.ory.sh/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5C6N2V97');`
+          }
         ],
       }
     },
