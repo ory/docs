5699c31d12b15c2172020cf839df2f070ae1c4c3
aeneasr <3372410+aeneasr@users.noreply.github.com>
2025-01-08 18:10:51 +0100

    chore: synchronize workspaces

--- code-examples/protect-page-login/nextjs-12/pages/_app.tsx
+++ code-examples/protect-page-login/nextjs-12/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css"
 
 // Use any because of React 18 breaking change: https://github.com/vercel/next.js/issues/36019
-export default function App({ Component, pageProps }: any) {
+export default function MyApp({ Component, pageProps }: any) {
   return <Component {...pageProps} />
 }
