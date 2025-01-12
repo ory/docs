66bbd37b92b353a816b497d4b75d7e6daec3c9cd
aeneasr <3372410+aeneasr@users.noreply.github.com>
2025-01-12 08:14:24 +0100

    chore: synchronize workspaces

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -2,17 +2,21 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import React from "react"
-import { QueryClient, QueryClientProvider, useQuery } from "react-query"
+import {QueryClient, QueryClientProvider, useQuery} from "react-query"
 import KapaWidget from "./KapaWidget"
+
 const queryClient = new QueryClient()
 
-function Root({ children }) {
+function Root({children}) {
   return (
     <>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-      <KapaWidget />
-    </>
-  )
+      <KapaWidget/>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `(function(){let k=window.Kapa;if(!k){let i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};window.Kapa=i}})();`
+        }}/>
+    </>)
 }
 
 export default Root
