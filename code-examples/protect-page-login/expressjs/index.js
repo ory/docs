d4a7b86c926b6911aaac47e44804dfcf6bc7548c
Christian Nwamba <nwambachristian@gmail.com>
2025-03-06 08:08:06 +0000

    chore: add line highlights and correct titles

--- code-examples/protect-page-login/expressjs/index.js
+++ code-examples/protect-page-login/expressjs/index.js
@@ -1,16 +1,22 @@
 const app = require("express")()
+// highlight-next-line
 const sdk = require("@ory/client-fetch")
 
+// highlight-start
 const ory = new sdk.FrontendApi(
   new sdk.Configuration({
     basePath: process.env.ORY_ENDPOINT || "http://localhost:4000/.ory",
   }),
 )
+ // highlight-end
+
 app.get("/", (req, res) =>
+  // highlight-start
   ory
     .toSession({ cookie: req.header("cookie") })
     .then((data) => res.json(data))
     .catch(() => res.redirect("/.ory/ui/login")),
+  // highlight-end
 )
 
 app.listen(3000, () => console.log("Server is running on port 3000"))
