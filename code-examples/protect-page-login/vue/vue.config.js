6a1929010ede1c27e2da760df6f578aa07d35f10
Christian Nwamba <nwambachristian@gmail.com>
2025-03-12 05:21:11 +0000

    refactor: migrate Vue.js example to Vite and update dependencies

--- code-examples/protect-page-login/vue/vue.config.js
+++
@@ -1,7 +0,0 @@
-// Copyright © 2022 Ory Corp
-// SPDX-License-Identifier: Apache-2.0
-
-const { defineConfig } = require("@vue/cli-service")
-module.exports = defineConfig({
-  transpileDependencies: true,
-})
