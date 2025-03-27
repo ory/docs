c029a1320f84bc17ca6537f804a7a05ba5119275
Christian Nwamba <nwambachristian@gmail.com>
2025-03-27 04:53:52 +0000

    feat: restructure landing   page (#2074)
    * feature: new docs welcome page
    
    * chore: format
    
    * feat: alt landing page
    
    * chore: format
    
    * feat: alt landing page 3
    
    * feat: landing page revamp
    
    * chore: format
    
    * fix: broken links and images
    
    * fix: missing icon
    
    * chore: update content
    
    * docs: landing page refinements
    
    * chore: format
    
    * fix: feedback changes
    
    * chore: format

--- src/sidebar.ts
+++ src/sidebar.ts
@@ -161,6 +161,7 @@ const cli: SidebarItemsConfig = [
 
 const quickstart: SidebarItemsConfig = [
   "welcome",
+  "intro",
   {
     type: "category",
     label: "Quickstart",
@@ -510,6 +511,10 @@ const oauth2: SidebarItemsConfig = [
       {
         type: "category",
         label: "Token management",
+        link: {
+          type: "generated-index",
+          slug: "guides/token-management",
+        },
         items: [
           "hydra/guides/oauth2-token-introspection",
           "oauth2-oidc/revoke-consent",
