1ce232aac6f72aed1a3742f7748cc3026c60a309
Christian Nwamba <nwambachristian@gmail.com>
2025-03-28 06:20:07 +0000

    chore: clean up example project

--- code-examples/protect-page-login/react/src/main.tsx
+++ code-examples/protect-page-login/react/src/main.tsx
@@ -1,15 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+// import App from './App.tsx'
 import AppWithAPI from './AppWithApi.tsx'
 import './assets/main.css'
-
-// Comment/uncomment to switch between components
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-//   <React.StrictMode>
-//     <App msg="Ory + React Example" />
-//   </React.StrictMode>,
-// )
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
