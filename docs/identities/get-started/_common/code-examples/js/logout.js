701a16e421b0f9f5fe7f0fe920f4cb3385da34bb
Christian Nwamba <nwambachristian@gmail.com>
2025-04-11 12:23:20 +0100

    chore: format

--- docs/identities/get-started/_common/code-examples/js/logout.js
+++ docs/identities/get-started/_common/code-examples/js/logout.js
@@ -1,13 +1,13 @@
 // Create logout route
 app.get("/logout", async (req, res) => {
-    try {
-        // Create a logout flow
-        const { logout_url } = await ory.createBrowserLogoutFlow({
-            cookie: req.header("cookie"),
-        });
-        // Redirect to logout URL
-        res.redirect(logout_url);
-    } catch (err) {
-        res.redirect("/");
-    }
-});
+  try {
+    // Create a logout flow
+    const { logout_url } = await ory.createBrowserLogoutFlow({
+      cookie: req.header("cookie"),
+    })
+    // Redirect to logout URL
+    res.redirect(logout_url)
+  } catch (err) {
+    res.redirect("/")
+  }
+})
