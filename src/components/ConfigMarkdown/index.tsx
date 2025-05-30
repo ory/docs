a68f74206d4f928cf5ef122ad2cd371215509c6f
vinckr <vincent@ory.sh>
2025-05-30 12:12:28 +0200

    chore: missing space

--- src/components/ConfigMarkdown/index.tsx
+++ src/components/ConfigMarkdown/index.tsx
@@ -236,7 +236,7 @@ export default function ConfigMarkdown(props: { src: string; binary: string }) {
       </Admonition>
       <p>
         To find out more about edge cases like setting string array values
-        through environmental variables head to the
+        through environmental variables head to the{" "}
         <a href={"/docs/ecosystem/configuring"}>Configuration</a> section.
       </p>
 
