cb07e32ac66beb21e58d25ce0da3696b3ee691b1
Parth <46842784+pi1814@users.noreply.github.com>
2025-11-12 20:06:24 +0530

    feat: flutter quickstart (#2241)
    * flutter quickstart update
    
    * updating docs to handle the .env misnaming in the pubspec and the port mismatch
    
    * resolving default import issue with mdx for .env ref
    
    * doc change for flutter quickstart
    
    * fix: format
    
    * update: pubspec.lock
    
    * update: pubspec
    
    * update: pubspec to higher version
    
    * update: packages
    
    * debug: test
    
    * debug: test
    
    * upgrade: flutter to latest version|
    
    * remove: web renderer option for flutter

---
+++ code-examples/protect-page-login/flutter_web_redirect/test/widget_test.dart
@@ -0,0 +1,34 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:dio/browser.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_web_redirect/main.dart';
+import 'package:flutter_web_redirect/services/auth.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    final dio = DioForBrowser();
+    final auth = AuthService(dio);
+    await tester.pumpWidget(MyApp(dio: dio, auth: auth));
+
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+  });
+}
