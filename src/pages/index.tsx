483ee611eaedc6664e7469aa1adc0c4b718ed582
unatasha8 <una.cogavin@ory.sh>
2025-12-10 09:32:00 -0800

    chore: adding make format changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
-import { useEffect } from 'react';
-import { useHistory } from '@docusaurus/router';
-import useBaseUrl from '@docusaurus/useBaseUrl';
+import { useEffect } from "react"
+import { useHistory } from "@docusaurus/router"
+import useBaseUrl from "@docusaurus/useBaseUrl"
 
 export default function Home() {
-  const history = useHistory();
-  const welcomeUrl = useBaseUrl('/welcome');
-  
+  const history = useHistory()
+  const welcomeUrl = useBaseUrl("/welcome")
+
   useEffect(() => {
-    history.replace(welcomeUrl);
-  }, [history, welcomeUrl]);
-  
-  return null;
+    history.replace(welcomeUrl)
+  }, [history, welcomeUrl])
+
+  return null
 }
