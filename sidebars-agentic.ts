cb0add94f1058aaf81ea8036cff91d752d70efe2
unatasha8 <una.cogavin@ory.sh>
2025-12-16 18:53:09 +0000

    docs: add sidebar-agentic

---
+++ sidebars-agentic.ts
@@ -0,0 +1,14 @@
+// Copyright © 2022 Ory Corp
+// SPDX-License-Identifier: Apache-2.0
+// sidebars-agentic.ts
+const agenticSidebar = [
+  {
+    type: "category",
+    label: "Agentic IAM",
+    items: [
+      "solutions/solution_agentic",
+    ],
+  },
+]
+
+export default agenticSidebar
