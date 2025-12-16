7b7584f3f782cba882cc1445e1f548116a22393c
unatasha8 <una.cogavin@ory.sh>
2025-12-16 18:54:19 +0000

    docs: added sidebars-b2b

---
+++ sidebars-b2b.ts
@@ -0,0 +1,14 @@
+// Copyright © 2022 Ory Corp
+// SPDX-License-Identifier: Apache-2.0
+// sidebars-b2b.ts
+const b2bSidebar = [
+  {
+    type: "category",
+    label: "B2B",
+    items: [
+      "solutions/solution_B2B",
+    ],
+  },
+]
+
+export default b2bSidebar
