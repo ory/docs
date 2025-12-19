189c6c86e447596cf5454aef58786bbe136f144b
Arne Luenser <arne.luenser@ory.sh>
2025-12-19 15:46:38 +0100

    feat: phrasing on OEL changelog when no changes are mentioned otherwise. (#2375)

--- src/components/ListChangelogs/ListChangelogs.jsx
+++ src/components/ListChangelogs/ListChangelogs.jsx
@@ -115,7 +115,10 @@ export default function ListChangelogs({ dir }) {
             {isNoChanges ? (
               <>
                 <Heading as="h2">{version}</Heading>
-                <p>No changes requiring a changelog in the {version}.</p>
+                <p>
+                  This version contains only minor changes and improvements such
+                  as dependency updates.
+                </p>
               </>
             ) : (
               <Comp />
