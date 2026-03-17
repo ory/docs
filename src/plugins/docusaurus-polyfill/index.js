e2b30c81383d4b8594dad8dd771a7ab5e805c414
unatasha8 <una.cogavin@ory.sh>
2026-03-17 10:11:28 -0700

    docs: updated plugins

--- src/plugins/docusaurus-polyfill/index.js
+++ src/plugins/docusaurus-polyfill/index.js
@@ -1,18 +1,29 @@
-const webpack = require("webpack")
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 
-// eslint-disable-next-line
 module.exports = function (context, options) {
   return {
     name: "docusaurus-polyfill",
-    // eslint-disable-next-line
     configureWebpack(config, isServer, utils) {
-      const plugins = [
-        new NodePolyfillPlugin(),
-      ]
-
       return {
-        plugins,
+        mergeStrategy: { "module.rules": "prepend" },
+        plugins: [
+          new NodePolyfillPlugin(),
+        ],
+        resolve: {
+          fallback: {
+            buffer: require.resolve("buffer/"),
+          },
+        },
+        module: {
+          rules: [
+            {
+              test: /\.m?js$/,
+              resolve: {
+                fullySpecified: false,
+              },
+            },
+          ],
+        },
       }
     },
   }
