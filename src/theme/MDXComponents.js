e30a9d580cd40c55883a2069295a5f006b952ffb
Wassim Bougarfa <12980387+wassimoo@users.noreply.github.com>
2026-05-13 12:21:43 +0200

    refactor: move RateLimitsTable import to its single usage site

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -4,7 +4,6 @@ import Tabs from "@theme/Tabs"
 import TabItem from "@theme/TabItem"
 import AjaxWarning from "./AjaxWarning"
 import ConsoleLink from "../components/ConsoleLink/console-link"
-import RateLimitsTable from "../components/RateLimitsTable"
 import SameDeploymentLink from "../components/SameDeploymentLink"
 
 export default {
@@ -12,7 +11,6 @@ export default {
   ...MDXComponents,
   AjaxWarning,
   ConsoleLink,
-  RateLimitsTable,
   SameDeploymentLink,
   Tabs,
   TabItem,
