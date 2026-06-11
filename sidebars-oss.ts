9ffba800faa24452034610a44c261721ab0ef6cd
Wassim Bougarfa <12980387+wassimoo@users.noreply.github.com>
2026-06-11 14:20:18 +0200

    fix: move kratos oss setup instruction path

--- sidebars-oss.ts
+++ sidebars-oss.ts
@@ -58,7 +58,7 @@ const ossSidebar = [
         className: "sidebar-icon sidebar-icon-kratos",
         items: [
           "oss/kratos/intro",
-          "kratos/install",
+          "oss/kratos/install",
           "kratos/guides/upgrade",
           "oss/kratos/quickstart",
           {
